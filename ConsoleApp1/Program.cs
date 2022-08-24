24328415570bf65543194833cdd92e9d17edb6c1
Venus <bisnesbrend@gmail.com>
2022-08-24 18:52:29 +0300

    ааауууу

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StradaniaGroup stradaniaGroup = new StradaniaGroup(new Student("Влад"), new Student("Лада"));
+
+            //IEnumerable myGroup = stradaniaGroup;
+
+            foreach(Student student in stradaniaGroup)
+            {
+                Console.WriteLine(student.FirstName);
+            }
+            Console.ReadKey();
+        }
+    }
+
+    public class Student
+    {
+        public string FirstName { get; set; }
+
+        public Student(string firstName)
+        {
+            this.FirstName = firstName;
+        }
+    }
+
+
+    public class StradaniaGroup : IEnumerable
+    {
+        Student[] moiRodnue;
+
+        public StradaniaGroup(params Student[] student)
+        {
+            moiRodnue = student;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return new StudentEnum(moiRodnue);
+
+        }
+    }
+
+
+
+    public class StudentEnum : IEnumerator
+    {
+        Student[] studentInGrop;
+
+        int position = -1;
+
+        public StudentEnum(Student[] studentInGrop)
+        {
+            this.studentInGrop = studentInGrop;
+        }
+
+        public bool MoveNext()
+        {
+            position++;
+
+            if(position < studentInGrop.Length)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void Reset()
+        {
+            position = -1;
+        }
+
+        public object Current
+        {
+            get
+            {
+                return studentInGrop[position];
+            }
+        }
+    }
+}
+
---
+++ ConsoleApp45/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp45
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable myLis = Metod();
+
+            foreach (var l in myLis)
+            {
+                Console.WriteLine(l);
+            }
+            Console.ReadKey();
+        }
+
+        static List<int> myList = new List<int> {-50, 5, 0, -1, 1, 2, 3 };
+
+
+        static IEnumerable Metod()
+        {
+            foreach(int element in myList )
+            {
+                if(element < 2)
+                {
+                    yield return element;
+                }
+                
+            }    
+        }
+    }
+}
